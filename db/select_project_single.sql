f6abc7dec413a2ad749f27789e5e9472d80409c0
Mika Be <mikushbe@gmail.com>
2022-02-27 21:17:36 -0500

    fix

--- db/select_project_single.sql
+++ db/select_project_single.sql
@@ -7,7 +7,7 @@ SELECT
     DATE_FORMAT(project_dt, '%b %d (%a)') as project_date,
     DATE_FORMAT(project_dt, '%l:%i %p') as project_time,
     project_location,
-    DATE_FORMAT(project_dt_proposed, '%b %d (%a)') as project_dt_proposed,
+    DATE_FORMAT(project_dt_proposed, '%Y-%m-%d') as project_dt_proposed,
     project_description,
     project_interest
 FROM 
